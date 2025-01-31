831e8c5cc76b54add63d3d2009c51c89ad7be81b
Dylan Barduzzi <dlbarduzzi@gmail.com>
2025-01-31 17:25:27 -0500

    Add site logo component

--- src/components/main-footer.tsx
+++ src/components/main-footer.tsx
@@ -2,7 +2,7 @@ import { Container } from "@/components/container"
 
 export function MainFooter() {
   return (
-    <footer className="border-t border-t-gray-200">
+    <footer className="border-t border-t-gray-200 bg-white">
       <Container>
         <div className="py-4">MainFooter</div>
       </Container>
--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -1,10 +1,29 @@
+import Link from "next/link"
+
+import { SiteLogo } from "@/components/site-logo"
 import { Container } from "@/components/container"
+
+import { cn } from "@/lib/utils"
 
 export function MainHeader() {
   return (
-    <header className="border-b border-b-gray-200">
+    <header className="border-b border-b-gray-200 bg-white">
       <Container>
-        <div className="flex h-16 items-center">MainHeader</div>
+        <div className="flex h-16 items-center justify-between gap-x-4">
+          <div className="flex items-center">
+            <Link
+              href="/"
+              className={cn(
+                "rounded-full focus-visible:outline-none focus-visible:ring-2",
+                "focus-visible:ring-black focus-visible:ring-offset-2"
+              )}
+            >
+              <SiteLogo />
+              <span className="sr-only">Link to home page.</span>
+            </Link>
+          </div>
+          <div>User</div>
+        </div>
       </Container>
     </header>
   )
