7b02de3230e6b950a118f536166859f2d44b0e3e
Dylan Barduzzi <dlbarduzzi@gmail.com>
2025-02-01 12:50:20 -0500

    Setup local docker database

--- README.md
+++ README.md
@@ -1,3 +1,53 @@
 # platvids
 
 A video platform designed to help you develop new skills.
+
+## Local development
+
+1. Install dependencies
+
+```sh
+npm install
+```
+
+2. Copy [.env.example](./.env.example) file into [.env](./.env) and update values accordingly.
+
+```sh
+cp .env.example .env
+```
+
+3. Start the database.
+
+```sh
+docker compose up
+```
+
+4. Create and/or migrate database schemas.
+
+TBD
+
+5. Start the application.
+
+```sh
+npm run dev
+```
+
+## Database helpers
+
+- Connect to docker database.
+
+```
+docker exec -it platvids-db-1 psql --host=localhost --dbname=${DB_NAME} --username=${DB_USER}
+```
+
+- View current user.
+
+```sql
+platvids=# SELECT current_user;
+```
+
+- Connect to database name.
+
+```sql
+platvids=# \c platvids;
+```
--- src/env/server.ts
+++ src/env/server.ts
@@ -6,6 +6,12 @@ import { createEnv } from "@t3-oss/env-nextjs"
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "production"]),
+    DB_HOST: z.string(),
+    DB_NAME: z.string(),
+    DB_USER: z.string(),
+    DB_PASS: z.string(),
+    DB_PORT: z.coerce.number(),
+    DATABASE_URL: z.string().url(),
   },
   onValidationError: (issues: readonly StandardSchemaV1.Issue[]) => {
     console.error("❌ Invalid server environment variables ❌", issues)
@@ -15,6 +21,12 @@ export const env = createEnv({
   runtimeEnv: {
     /* eslint-disable n/no-process-env */
     NODE_ENV: process.env.NODE_ENV,
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_NAME: process.env.DB_NAME,
+    DB_USER: process.env.DB_USER,
+    DB_PASS: process.env.DB_PASS,
+    DATABASE_URL: process.env.DATABASE_URL,
     /* eslint-enable n/no-process-env */
   },
   emptyStringAsUndefined: true,
