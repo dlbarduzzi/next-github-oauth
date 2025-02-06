8bb777acd14c05cf36aaae060c233442a69a1c8a
Dylan Barduzzi <dlbarduzzi@gmail.com>
2025-02-06 09:09:06 -0600

    Update sidebar with icons

--- src/app/(courses)/courses/layout.tsx
+++ src/app/(courses)/courses/layout.tsx
@@ -1,12 +1,38 @@
-import { cn } from "@/lib/utils"
 import type { ReactNode } from "react"
+
+import { Calendar, ChartPie, FileText, FolderOpen, House, Settings } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+
+const sidebarNav = [
+  {
+    name: "Dashboard",
+    icon: House,
+  },
+  {
+    name: "Projects",
+    icon: FolderOpen,
+  },
+  {
+    name: "Calendar",
+    icon: Calendar,
+  },
+  {
+    name: "Documents",
+    icon: FileText,
+  },
+  {
+    name: "Reports",
+    icon: ChartPie,
+  },
+]
 
 function Sidebar() {
   return (
     <div
       className={cn(
         "hidden bg-white sm:fixed sm:inset-y-0 sm:z-40 sm:flex",
-        "sm:w-20 sm:flex-col lg:w-72"
+        "sm:w-20 sm:flex-col lg:w-64"
       )}
     >
       <div
@@ -17,18 +43,21 @@ function Sidebar() {
       >
         <nav className="flex h-full flex-col gap-y-4 pt-4">
           <div className="flex flex-1 flex-col gap-y-4">
-            {Array.from({ length: 5 }).map((_, index) => (
+            {sidebarNav.map((item, index) => (
               <div key={index} className="lg:flex lg:items-center lg:gap-x-3">
                 <button
                   type="button"
                   className={cn(
-                    "flex h-11 w-[2.95rem] items-center justify-center rounded-md",
-                    "bg-gray-200 px-4 py-2 text-sm transition-colors hover:bg-gray-300"
+                    "group flex h-11 w-[2.95rem] items-center justify-center",
+                    "rounded-md bg-transparent text-sm transition-colors",
+                    "hover:bg-gray-200"
                   )}
                 >
-                  S
+                  <item.icon
+                    className={cn("size-5 text-gray-600 group-hover:text-gray-800")}
+                  />
                 </button>
-                <span className="hidden lg:inline">Settings</span>
+                <span className="hidden lg:inline">{item.name}</span>
               </div>
             ))}
           </div>
@@ -36,11 +65,12 @@ function Sidebar() {
             <button
               type="button"
               className={cn(
-                "flex h-11 w-[2.95rem] items-center justify-center rounded-md",
-                "bg-gray-200 px-4 py-2 text-sm transition-colors hover:bg-gray-300"
+                "group flex h-11 w-[2.95rem] items-center justify-center",
+                "rounded-md bg-transparent text-sm transition-colors",
+                "hover:bg-gray-200"
               )}
             >
-              B
+              <Settings className="size-5 text-gray-600 group-hover:text-gray-800" />
             </button>
             <span className="hidden lg:inline">Profile</span>
           </div>
@@ -63,7 +93,7 @@ function Header() {
 
 function Footer() {
   return (
-    <footer className="border-t border-t-gray-200 bg-white sm:pl-20 lg:pl-72">
+    <footer className="border-t border-t-gray-200 bg-white sm:pl-20 lg:pl-64">
       <div className="p-4">Footer</div>
     </footer>
   )
@@ -74,7 +104,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <div className="flex min-h-screen flex-col">
       <Sidebar />
       <Header />
-      <main className="grid flex-1 pt-16 sm:pl-20 lg:pl-72">{children}</main>
+      <main className="grid flex-1 pt-16 sm:pl-20 lg:pl-64">{children}</main>
       <Footer />
     </div>
   )
